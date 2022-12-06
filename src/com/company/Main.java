479badcebdae588119dafc6e5c4eb1c677870dd9
Регина <rmashkovtseva@gmail.com>
2022-12-06 23:01:09 +0300

    Добавляем 3 метода: 1. Из строки превращаем в double значение 2. Площадь круга 3. Длина окружности запуск программы.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    public static double pi = 3.14;
+
+    public static double lineToNumeric(String str){
+        double okr = 0;
+        try{
+           okr = Double.parseDouble(str);
+        }catch(NumberFormatException e){
+            System.out.println("Вы не верно ввели размер окружности");
+        }
+        return okr;
+    }
+
+    public static void area(double okr){
+        System.out.println("Площадь окружности равна: "+(okr*okr)*pi);
+    }
+
+    public static void circle (double okr){
+        System.out.println("Длина окружности равна: "+2*pi*okr);
+    }
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner scan = new Scanner(System.in);
+        System.out.println("Введите радиус круга: ");
+        String line = scan.nextLine();
+        area(lineToNumeric(line));
+        circle(lineToNumeric(line));
+
     }
 }
